d92913e8b3cdb07af42863df997e0393a891709f
Alina <67075021+alinabudukina@users.noreply.github.com>
2021-07-16 12:02:50 +0200

    tailwind settings added + user controller created

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+  <!-- wrapper -->
+<section class="m-2 font-sans leading-normal flex">
+
+  <!-- card container -->
+  <div class="max-w-lg shadow-lg rounded overflow-hidden m-4 sm:flex">
+    <div class="h-48 sm:h-full:w-64 md:w-64 flex-none bg-cover bg-center rounded rounded-t sm:rounded sm:rounded-l text-center overflow-hidden" style="background-image: url('https://unsplash.it/804/800')">
+    </div>
+    
+    <!-- card-content -->
+     <div class="px-6 py-4">
+      <h2 class="mb-2 font-black">{{$post->user->name}}</h2>
+      <p class="mb-4 text-grey-dark text-sm">
+      {{$post->description}}
+       </p>
+      
+       <!-- button -->
+      <!-- <button class="py-3 px-6 bg-purple hover:bg-purple-light text-white font-bold rounded-full mt-1 mb-2">
+        Hey, click me.
+       </button> -->
+       
+    </div>
+
+  </div>
+  
+</section>
+</body>
+</html>
